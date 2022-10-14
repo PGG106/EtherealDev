dab7162d09c9d30930cf885488340f09d806a305
PGG106 <g.pagano45@studenti.unisa.it>
2022-10-14 16:27:27 +0200

    Bench: 4640448

--- src/search.c
+++ src/search.c
@@ -460,7 +460,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
         && !inCheck
         && !ns->excluded
         &&  depth <= BetaPruningDepth
-        &&   ns->eval - BetaMargin * depth > beta)
+        &&  eval - BetaMargin * depth > beta)
         return eval;
 
     // Step 8 (~3 elo). Alpha Pruning for main search loop. The idea is
